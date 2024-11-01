c3ff81c8e443487ce0ce10d02c3d13f9cbc809eb
Joseph Carl <josephic9617@gmail.com>
2024-11-01 20:48:34 +0500

    some issues fixed

--- hakhyzmat_project/settings.py
+++ hakhyzmat_project/settings.py
@@ -77,11 +77,11 @@ WSGI_APPLICATION = 'hakhyzmat_project.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': env('DB_NAME'),
-        'USER': env('DB_USER'),
-        'PASSWORD': env('DB_PASSWORD'),
-        'HOST': env('DB_HOST', default='localhost'),
-        'PORT': env('DB_PORT', default='5432'),
+        'NAME': env.str('DB_NAME'),
+        'USER': env.str('DB_USER'),
+        'PASSWORD': env.str('DB_PASSWORD'),
+        'HOST': env.str('DB_HOST', default='localhost'),
+        'PORT': env.str('DB_PORT', default='5432'),
 
         # 'ENGINE': 'django.db.backends.sqlite3',
         # 'NAME': BASE_DIR / 'db.sqlite3',
